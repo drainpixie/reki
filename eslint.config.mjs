7598b1fc1eb186ec22a9eaf6040b9ff46fdae11f
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-28 12:55:26 +0200

    feat: ignore path in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath, URL } from "node:url";
+
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-);
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default [
+  includeIgnoreFile(gitignorePath),
+
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+  ),
+];
