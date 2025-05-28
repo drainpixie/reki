5a34879fc8ae02728263cee21d8c951ff857bf24
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-05-28 14:42:44 +0200

    build: eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,16 @@ import { includeIgnoreFile } from "@eslint/compat";
 import { fileURLToPath, URL } from "node:url";
 
 import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
-export default [
+export default tseslint.config(
   includeIgnoreFile(gitignorePath),
 
-  ...tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.strict,
-    tseslint.configs.stylistic,
-  ),
-];
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  prettier,
+);
