c81605286ec47d770baf39c063b4a579909e0617
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-11-26 16:23:24 +0100

    feat: update eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,18 @@
-// @ts-check
-
 import { includeIgnoreFile } from "@eslint/compat";
 import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "eslint/config";
 
 import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
-
-export default tseslint.config(
-  includeIgnoreFile(gitignorePath),
+export default defineConfig([
+  includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
 
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+
   prettier,
-);
+]);
